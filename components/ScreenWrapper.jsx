e0e89271fd3f7e035f03e21adabd27d58d0055b0
Moksh Keloo <moksh@integratedwebworks.com>
2024-12-19 16:46:43 -0500

    Add initial project structure with core components and constants

---
+++ components/ScreenWrapper.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ScreenWrapper = ({ children, bg}) => {
+  return (
+    <View>
+         { children}
+    </View>
+  );
+};
+
+export default ScreenWrapper;
